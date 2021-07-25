10d25531213181e1fd8363861afa07ce5a48ddb7
ThiraTheNerd <thiragithinji@gmail.com>
2021-07-26 02:25:10 +0300

    add the configuration for .env file

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -20,7 +20,11 @@ import django_heroku
 import sys
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+cloudinary.config( 
+  cloud_name = config('CLOUD_NAME'), 
+  api_key = config('API_KEY'),
+  api_secret = config('API_SECRET')
+)
 cloudinary.config(
     cloud_name='playboard',
     api_key=742327432827324,
@@ -62,6 +66,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'neighbourhood.urls'
